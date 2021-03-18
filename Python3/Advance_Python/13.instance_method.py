efcad631fb3798ace75ba152616b67821069e31f
Termux <artificialworld09@gmail.com>
2021-03-19 00:27:21 +0300

    updated: 'Python3' dir

--- Python3/Advance_Python/13.instance_method.py
+++ Python3/Advance_Python/13.instance_method.py
@@ -10,13 +10,37 @@ There are two types of instance_methods
 
 Instance methods are the methods which act upon the instance variables of the class.
 Instance_method need to know the memory address of the instance which is provided through self variable by default as first parameter of the instance_method.
+
+Instance_methods are bound to object of the class so we call instance_method with object name.
 2:30/13:50
 '''
 
 ##Instance_method without Parameter/Formal_argument
-def method_name(self):
-	function body
+#class Mobile():
+#	def show(self):
+#		print('RealMe X')
+#		
+#realme = Mobile()
+#realme.show()
+
+
+#class Mobile():
+#	def __init__(self):
+#		self.model = 'RealMe X' #instance_variable
+#	def show(self): #instance_method
+#		print('Model:', self.model)
+#		
+#realme = Mobile()
+#realme.show()
 
 ##Instance_method with Parameter/Formal_argument
-def method_name(self, f1, f2):
-	function body
+class Mobile():
+	def __init__(self):
+		self.model = 'RealMe X' #instance_variable
+	def show(self, p): #instance_method
+		self.price = p
+		print('Model:', self.model)
+		print('Price:', self.price)
+		
+realme = Mobile()
+realme.show(5000)
