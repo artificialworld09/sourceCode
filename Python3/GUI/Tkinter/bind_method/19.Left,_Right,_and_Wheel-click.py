6976cfb9661b8a5ecee1500759e025cfb40bae58
vBoxKali <artificialworld09@gmail.com>
2021-03-19 05:45:15 +0530

    updated: 'Python3/GUI' dir

--- Python3/GUI/Tkinter/bind_method/19.Left,_Right,_and_Wheel-click.py
+++ Python3/GUI/Tkinter/bind_method/19.Left,_Right,_and_Wheel-click.py
@@ -1,7 +1,7 @@
 from tkinter import*
 root = Tk()
 root.title("Home")
-pt = PhotoImage(file = 'images/css.png')
+pt = PhotoImage(file = '../images/css.png')
 root.iconphoto(False, pt)
 root.geometry("1000x400+1820+0")
 root.resizable(0,0)
--- Python3/GUI/Tkinter/bind_method/29.Click_and_Release.py
+++ Python3/GUI/Tkinter/bind_method/29.Click_and_Release.py
@@ -1,7 +1,7 @@
 from tkinter import*
 root = Tk()
 root.title("Home")
-pt = PhotoImage(file = 'images/css.png')
+pt = PhotoImage(file = '../images/css.png')
 root.iconphoto(False, pt)
 root.geometry("1000x400+1820+0")
 root.resizable(0,0)
@@ -10,14 +10,11 @@ root.config(bg="#262626")
 def red(r):
     root.config(bg="red")
 def yellow(y):
-    root.config(bg="yello")
-def blue(b):
-    root.config(bg="blue")
+    root.config(bg="yellow")
 
 b1 = Button(root, text="Green",font=("times new roman",15,"bold"),bg="green",fg="white",activebackground="blue",activeforeground="red",cursor="hand2")
-b1.bind("<Double-1>", red) #Left_click
-b1.bind("<Double-2>", yellow) #Wheel_click
-b1.bind("<Double-3>", blue) #Right_click
+b1.bind("<Button>", red) #on Click
+b1.bind("<ButtonRelease>", yellow) #on Release
 
 b1.place(x=10,y=70,width=150,height=28)
 
