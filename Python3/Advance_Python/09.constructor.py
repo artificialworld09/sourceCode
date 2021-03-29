45be7b893481fe1d2a3d6c54de59ce0a0a3e85ed
Termux <artificialworld09@gmail.com>
2021-03-29 20:08:04 +0300

    updated: 'Python3/Advance_Python/09.constructor.py' file

--- Python3/Advance_Python/09.constructor.py
+++ Python3/Advance_Python/09.constructor.py
@@ -2,12 +2,16 @@
 #	def __init__(self):
 #		print("Mobile constructor called")
 #realme = Mobile()
+#redmi = Mobile()
+#oppo = Mobile()
+
 
 
 #class Mobile:
 #	## constructor without parameter
 #	def __init__(self):
 #		self.model = "Realme X"
+#		
 #	def show(self):
 #		print("Model: ", self.model)
 #realme = Mobile()
@@ -16,46 +20,39 @@
 
 
 #class Mobile:
-#	def __init__(self, m, v=80):
-#			self.model = m
-#			self.volumn = v
-#	def show(self, p):
-#			price = p #local variable
-#			print(f"Model: {self.model}\nVolumn: {self.volumn}\nPrice: {price}")
-
-## passing argument to constructor
-#realme = Mobile("Realme X")
-
-## accessing method from outside class
-#realme.show(5000)
+#	## constructor with parameter
+#	def __init__(self, m, p):
+#		self.model = m
+#		self.price = p
+#		
+#	def show(self):
+#		print(f"Model: {self.model}\nPrice: {self.price}")
+#		print()
+#		
+##passing argument to constructor		
+#realme = Mobile("Realme X", 5500)
+#nokia = Mobile("Nokia 3310", 2400)
+##accessing method from outside class
+#realme.show()
+#nokia.show()
 
 
 
 #class Mobile:
+#	## constructor with default parameter
 #	def __init__(self, m, v=80):
 #			self.model = m
 #			self.volumn = v
+#			
 #	def show(self, p):
 #			price = p #local variable
 #			print(f"Model: {self.model}\nVolumn: {self.volumn}\nPrice: {price}")
+#			print()
 
-## passing argument to constructor
-#realme = Mobile("Realme X", 90)
-
-## accessing method from outside class
+##passing argument to constructor
+#realme = Mobile("Realme X") #not passing argument for v
+##accessing method from outside class
 #realme.show(5000)
 
-
-
-
-class Mobile:
-	## constructor with parameter
-	def __init__(self, m, p):
-		self.model = m
-		self.price = p
-	def show(self):
-		print(f"Model: {self.model}\nPrice: {self.price}")
-realme = Mobile("Realme X", 5500)
-nokia = Mobile("Nokia 3310", 2400)
-realme.show()
-nokia.show()
+#redmi = Mobile("Redmi 7", 50) #passing argument for v
+#redmi.show(8000)
