946dce46b957b8d57075e0a9943863d989f3f955
vBoxKali <artificialworld09@gmail.com>
2021-04-09 15:27:39 +0530

    updated: 'Python3' dirs

---
+++ Python3/Advance_Python/112.Exception_Handling_and_Built-in_Exception.py
@@ -0,0 +1,11 @@
+'''
+An exception is a runtime error which can be handled by the programmer.
+All exceptions are represented as classes in Python.
+
+There are two types of Exception:-
+Built-in Exception: Exception which are already available in Python Language. The base class for all built-in exceptions is BaseException class.
+
+User Defined Exception: A programmer can create  his own exceptions, called user-defined exceptions.
+
+1:23/37:16
+'''
