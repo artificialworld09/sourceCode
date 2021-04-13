0536c7b8e43ce8cdb09eba676a182826a2daf01d
Termux <artificialworld09@gmail.com>
2021-04-13 12:31:49 +0300

    updated: 'Python3/Advanced_Python' dir

---
+++ Python3/Advance_Python/42.time_class.py
@@ -0,0 +1,8 @@
+'''
+time object: A time object is an object containing information of local time of day, independent of any particular day, and subject to adjustment via tzinfo object.
+
+CREATING OBJECT OF TIME CLASS
+object_name = time(hour=0, minut=0, second=0, microsecond=0, tzinfo=None, *, fold=0)
+
+All arguments are optional
+'''
