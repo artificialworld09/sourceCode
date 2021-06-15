4d0e9fe0239c008cd736bcc9e2f11698289b1d2c
Artificialworld01 <artificialworld09@gmail.com>
2021-06-16 01:01:59 +0300

    updated: 'Python3/Core_Python/module/Gtts/speak.py' file

--- Python3/Core_Python/module/Gtts/speak.py
+++ Python3/Core_Python/module/Gtts/speak.py
@@ -1,30 +1,30 @@
 ## basic code(Hindi)
-#import os
-#from gtts import gTTS
+# import os
+# from gtts import gTTS
 
-#def speak(text):
+# def speak(text):
 #    tts = gTTS(text=text, lang='hi')
 #    filename = 'audios/'+text+'.mp3'
 #    tts.save(filename)
-#        
-#data='मेरा नाम एलेक्सा है'
-#speak(data)
+       
+# data='मेरा नाम एलेक्सा है'
+# speak(data)
 
 
 
 
 ## basic code(English)
-#import os
-#from gtts import gTTS
+# import os
+# from gtts import gTTS
 
-#def speak(text):
-#    tts = gTTS(text=text, lang='en-in')
-#    filename = 'audios/'+text+'.mp3'
+# def speak(text):
+#    data=text.lower()
+#    tts = gTTS(text=data, lang='en-in')
+#    filename = 'audios/'+data+'.mp3'
 #    tts.save(filename)
-#        
-#st='My NAMe Is AleXa'
-#data=st.lower()
-#speak(data)
+       
+# data='My NAMe Is AleXa'
+# speak(data)
     
     
 
@@ -32,20 +32,20 @@
 
 
 ##Final
-#import os
-#from gtts import gTTS
-#from playsound import playsound
+import os
+from gtts import gTTS
+from playsound import playsound
 
-#def speak(text):
-#    tts = gTTS(text=text, lang='en-in')
-#    filename = 'audios/'+text+'.mp3'
-#    if os.path.exists(filename):
-#        playsound(filename)
-#    else:
-#        tts.save(filename)
-#        playsound(filename)
-#        
-#if(__name__=='__main__'):
-#    st='My NAMe Is AleXa'
-#    data = st.lower()
-#    speak(data)
+def speak(text):
+   data = text.lower()
+   tts = gTTS(text=data, lang='en-in')
+   filename = 'audios/'+data+'.mp3'
+   if os.path.exists(filename):
+       playsound(filename)
+   else:
+       tts.save(filename)
+       playsound(filename)
+       
+if(__name__=='__main__'):
+   data='My NAMe Is AleXa'
+   speak(data)
