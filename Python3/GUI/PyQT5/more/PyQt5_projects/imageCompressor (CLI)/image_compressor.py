75545f8b95d19848ba73499fcadd8b906b1f6880
Artificialworld01 <artificialworld09@gmail.com>
2021-07-18 11:44:14 +0300

    added: 'web_designing/CSS/css_grid-system' dir

--- Python3/GUI/PyQT5/more/PyQt5_projects/imageCompressor (CLI)/image_compressor.py
+++ Python3/GUI/PyQT5/more/PyQt5_projects/imageCompressor (CLI)/image_compressor.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 myWidth=3264
 
-img=Image.open('me.jpg')
+img=Image.open('a.jpg')
 wpercent=(myWidth/float(img.size[0]))
 hsize=int((float(img.size[1])*float(wpercent)))
 img=img.resize((myWidth,hsize), PIL.Image.ANTIALIAS)
